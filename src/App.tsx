466aa12598255fdf291c4e2cf1d8b2df9decaca3
Michael Moore <mmoore4361@gmail.com>
2022-02-22 09:20:08 +0200

    random change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="text-center">
-    <div className="flex min-h-screen justify-center items-center">
-        <h1 className="">
-            Template
-        </h1>
-    </div>
-    </div>
-  );
+    return (
+        <div className="text-center">
+            <div className="flex min-h-screen justify-center items-center">
+                <h1 className="">Template</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
