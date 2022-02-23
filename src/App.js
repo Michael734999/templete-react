368ca554bbe6d09f9e16dbbf4902e82a57cd92c8
Michael Moore <mmoore4361@gmail.com>
2022-02-23 16:06:44 +0200

    change layouts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Footers from './components/footer/footers';
+import Header from './components/header/Header';
 
 function App() {
     return (
-        <div className="text-center">
-            <div className="flex min-h-screen justify-center items-center">
-                <h1 className="">Template</h1>
+        <div>
+            <Header />
+            <div className="text-center">
+                <div className="flex min-h-screen justify-center items-center">
+                    <h1 className="">Template</h1>
+                </div>
             </div>
+            <Footers />
         </div>
     );
 }
